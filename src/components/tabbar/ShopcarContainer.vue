792f6984d89020b52a4861c713c7806d48430d9f
xcy <779090286@qq.com>
2019-06-05 19:11:22 +0800

    完成购物车，加入状态管理的vuex

--- src/components/tabbar/ShopcarContainer.vue
+++ src/components/tabbar/ShopcarContainer.vue
@@ -1,15 +1,81 @@
 <template>
-  <div>
-    <h1>ShopcarContainer</h1>
+  <div class="shopcar-container">
+    <div class="goods-list">
+      <div class="mui-card">
+        <div class="mui-card-content">
+          <div class="mui-card-content-inner">
+            <mt-switch></mt-switch>
+            <img src="//i1.mifile.cn/a1/pms_1524621350.77238418!220x220.jpg">
+            <div class="info">
+              <h1>一代神机小米6，全网通版64G</h1>
+              <p>
+                <span class="price">￥1999</span>
+                <numbox :initcount="$store.getters.getGoodsCount['90']"></numbox>
+                <a>删除</a>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="mui-card">
+      <div class="mui-card-content">
+        <div class="mui-card-content-inner jiesuan">
+          <div class="left">
+            <p>总计（不含运费）</p>
+            <p>已勾选商品 <span class="red">0</span> 件，总价 <span class="red">￥0</span></p>
+          </div>
+          <mt-button type="danger">结算</mt-button>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-    export default {
-        name: "ShopcarContainer"
+  import numbox from '../subcomponents/shopcar_numbox'
+
+  var obj = localStorage.getItem('cart')
+
+  export default {
+    name: "ShopcarContainer",
+    components:{
+      numbox
     }
+  }
 </script>
 
-<style scoped>
-
+<style scoped lang="scss">
+.shopcar-container{
+  background-color: #eeeeee;
+  overflow: hidden;
+  .goods-list{
+    .mui-card-content-inner{
+      display: flex;
+    }
+    img{
+      width: 60px;
+      height: 60px;
+    }
+    h1{
+      font-size: 14px;
+    }
+    .info{
+      display: flex;
+      flex-direction: column;
+      .price{color:red;font-weight: bold}
+    }
+  }
+  .jiesuan{
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    .red{
+      color: red;
+      font-weight: bold;
+      font-size: 16px;
+    }
+  }
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -13,13 +13,116 @@ import './lib/mui/css/icons-extra.css'
 // 导入axios
 import axios from 'axios'
 import moment from 'moment'
+// 注册vuex
+import Vuex from 'vuex'
 
 axios.defaults.baseURL = 'http://127.0.0.1:5000/';
 axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
 Vue.config.productionTip = false
 Vue.use(VueRouter)
 Vue.use(MintUI)
+Vue.use(Vuex)
 Vue.prototype.$http = axios
+
+var obj = [{"id":90,"count":3,"price":1999,"seleted":true}]
+obj = JSON.stringify(obj)
+localStorage.setItem('cart', obj)
+var cart = JSON.parse(localStorage.getItem('cart')) || '[]'
+
+// 创建vuex对象
+var store = new Vuex.Store({
+  state:{
+    cart: cart
+  },
+  mutations:{ // 使用this.$store.commit('方法名')调用
+    addToCart(state, goodsinfo){
+      var flag = false
+
+      state.cart.some(item => {
+        if(item.id === goodsinfo.id){
+          item.count += parseInt(goodsinfo.count)
+          flag = true
+          return true
+        }
+      })
+
+      if(!flag){
+        state.cart.push(goodsinfo)
+      }
+
+      localStorage.setItem('cart', JSON.stringify(state.cart))
+    },
+    updateGoodsInfo(state, goodsinfo) {
+      // 修改购物车中商品的数量值
+      // 分析：cart
+      state.cart.some(item => {
+        if (item.id === goodsinfo.id) {
+          item.count = parseInt(goodsinfo.count)
+          return true
+        }
+      })
+      // 当修改完商品的数量，把最新的购物车数据，保存到 本地存储中
+      localStorage.setItem('cart', JSON.stringify(state.cart))
+    },
+    removeFormCar(state, id) {
+      // 根据Id，从store 中的购物车中删除对应的那条商品数据
+      state.cart.some((item, i) => {
+        if (item.id === id) {
+          state.cart.splice(i, 1)
+          return true;
+        }
+      })
+      // 将删除完毕后的，最新的购物车数据，同步到 本地存储中
+      localStorage.setItem('cart', JSON.stringify(state.cart))
+    },
+    updateGoodsSelected(state, info) {
+      state.cart.some(item => {
+        if (item.id === info.id) {
+          item.selected = info.selected
+        }
+      })
+      // 把最新的 所有购物车商品的状态保存到 store 中去
+      localStorage.setItem('cart', JSON.stringify(state.cart))
+    }
+  },
+  getters:{
+        // 相当于 计算属性，也相当于 filters
+    getAllCount(state) {
+      var c = 0;
+      state.cart.forEach(item => {
+        c += item.count
+      })
+      return c
+    },
+    getGoodsCount(state) {
+      var o = {}
+      state.cart.forEach(item => {
+        o[item.id] = item.count
+      })
+      return o
+    },
+    getGoodsSelected(state) {
+      var o = {}
+      state.cart.forEach(item => {
+        o[item.id] = item.selected
+      })
+      return o
+    },
+    getGoodsCountAndAmount(state) {
+      var o = {
+        count: 0, // 勾选的数量
+        amount: 0 // 勾选的总价
+      }
+      state.cart.forEach(item => {
+        if (item.selected) {
+          o.count += item.count
+          o.amount += item.price * item.count
+        }
+      })
+      return o
+    }
+  }
+})
 
 // 定义全局的过滤器
 Vue.filter('dateFormat', function (dataStr, pattern = 'YYYY-MM-DD') {
@@ -29,5 +132,6 @@ Vue.filter('dateFormat', function (dataStr, pattern = 'YYYY-MM-DD') {
 new Vue({
   el: '#app',
   router,
-  render: c => c(App)
+  render: c => c(App),
+  store
 })
